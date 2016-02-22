94868677b78a9b9e24a24b7a035a11df6bd86046
kazeshan <kazeshan@gmail.com>
2016-02-22 12:31:22 +0800

    add commandBar v0.0

---
+++ test/src/userInterface/CommandBar.java
@@ -0,0 +1,54 @@
+package userInterface;
+
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.GridPane;
+
+public class CommandBar {
+
+    private GridPane _mainPane;
+    private TextField _textField;
+    private int _numberOfItems = 0;
+
+    public CommandBar() {
+        initializeMainPane();
+        initializeTextBox();
+        _mainPane.add(_textField, _numberOfItems++, 0);
+    }
+
+    public void initializeMainPane() {
+        _mainPane = new GridPane();
+        _mainPane.setMaxHeight(30);
+        _mainPane.setPadding(new Insets(4, 2, 0, 2));
+        _mainPane.setStyle("-fx-background-color: #FFFFFF;");
+        _mainPane.setAlignment(Pos.CENTER);
+    }
+
+    public void initializeTextBox() {
+        _textField = new TextField();
+        _textField.setId("mainUserInput");
+        _textField.setPrefWidth(800.0);
+        _textField.setBorder(null);
+    }
+
+    public void setTextFieldHandler(EventHandler<KeyEvent> mainEventHandler) {
+        _textField.setOnKeyPressed(mainEventHandler);
+    }
+
+    public void focus() {
+        _mainPane.requestFocus();
+        _textField.requestFocus();
+    }
+
+    public TextField getTextField() {
+        return _textField;
+    }
+
+    public GridPane getCommandBar() {
+        return _mainPane;
+    }
+
+}
