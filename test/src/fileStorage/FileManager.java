d13c9d3e1e8a3b5dbd0d5d9cf5ecc9d632d0fe75
Angie <angietio94@gmail.com>
2016-02-27 11:06:19 +0800

    Added constructor, createNewFile() and readFromExistingFile()

--- test/src/fileStorage/FileManager.java
+++ test/src/fileStorage/FileManager.java
@@ -1,5 +1,36 @@
 package fileStorage;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.RandomAccessFile;
+
 public class FileManager {
+    
+    private RandomAccessFile file;
+    private String fileName;
+    private BufferedReader buffer;
+    
+    public FileManager() {
+        fileName = "taskList.txt";
+    }
+    
+    private void createNewFile() {
+        try {
+            file = new RandomAccessFile(fileName, "rw");
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+            e.printStackTrace();
+        }
+    }
+    
+    private void readFromExistingFile() {
+        try {
+            buffer = new BufferedReader(new FileReader(fileName));
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+            e.printStackTrace();
+        }
+    }
     
 }
