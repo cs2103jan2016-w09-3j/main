a2461c592c4c93133932744d78d7805309cd132b
Ten Zhi Yang <tzyinc@hotmail.com>
2016-03-01 18:52:16 +0800

    Added InputParser

---
+++ test/src/dateParser/InputParser.java
@@ -0,0 +1,20 @@
+package dateParser;
+
+public class InputParser {
+	private String input;
+	
+	
+	public InputParser(String input){
+		
+	}
+
+
+	public String getInput() {
+		return input;
+	}
+
+
+	public void setInput(String input) {
+		this.input = input;
+	}
+}
