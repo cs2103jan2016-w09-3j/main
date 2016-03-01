143c15760173a5bd7f880a94a10e863bf1bcedd6
Ten Zhi Yang <tzyinc@hotmail.com>
2016-03-01 21:21:44 +0800

    Added Information Parser

---
+++ test/src/dateParser/InformationParser.java
@@ -0,0 +1,22 @@
+package dateParser;
+
+public class InformationParser {
+	private String Title;
+	private String Description;
+
+	public String getTitle() {
+		return Title;
+	}
+
+	public void setTitle(String title) {
+		Title = title;
+	}
+
+	public String getDescription() {
+		return Description;
+	}
+
+	public void setDescription(String description) {
+		Description = description;
+	}
+}
