4f6352573c6eca44a4433b4d14115745774a51e0
Angie <angietio94@gmail.com>
2016-03-01 09:07:48 +0800

    Added convertToJson and extractDetails

--- test/src/fileStorage/FileManager.java
+++ test/src/fileStorage/FileManager.java
@@ -4,15 +4,44 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.RandomAccessFile;
+import java.util.ArrayList;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import entity.TaskEntity;
 
 public class FileManager {
     
     private RandomAccessFile file;
     private String fileName;
     private BufferedReader buffer;
+    private ArrayList<TaskEntity> mainTaskEntities;
+    private ArrayList<TaskEntity> floatingTaskEntities;
     
-    public FileManager() {
+    public FileManager(ArrayList<TaskEntity> _main, ArrayList<TaskEntity> _floating) {
         fileName = "taskList.txt";
+        mainTaskEntities = _main;
+        floatingTaskEntities = _floating;
+    }
+    
+    public void extractDetails() throws JSONException {
+        for(int i = 0; i< mainTaskEntities.size(); i++) {
+            convertToJson(i);
+        }
+    }
+    
+    public void convertToJson(int i) throws JSONException {
+        JSONObject taskDetails = new JSONObject();
+        
+        taskDetails.put("id", mainTaskEntities.get(i).getId());
+        taskDetails.put("name", mainTaskEntities.get(i).getName());
+        taskDetails.put("dueDate", mainTaskEntities.get(i).getDueDate());
+        taskDetails.put("dateCreated", mainTaskEntities.get(i).getDateCreated());
+        taskDetails.put("description", mainTaskEntities.get(i).getDescription());
+        taskDetails.put("floatingTask", mainTaskEntities.get(i).isFloating());
+        
+        writeToFile(taskDetails);
     }
     
     private void createNewFile() {
@@ -33,4 +62,7 @@ public class FileManager {
         }
     }
     
+    private void writeToFile(JSONObject task) {
+        // TODO
+    }
 }
