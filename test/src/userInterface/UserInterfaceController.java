492d79f626805df75b9293692a40d655c9efe0aa
kazeshan <kazeshan@gmail.com>
2016-03-08 19:58:58 +0800

    remove debuggin lines

--- test/src/userInterface/UserInterfaceController.java
+++ test/src/userInterface/UserInterfaceController.java
@@ -208,15 +208,12 @@ public class UserInterfaceController {
 
 		int insertedTo = _taskManager.add(task);
 		int selected = _taskViewInterface.getSelectIndex();
-		System.out.println("id "+selected+" "+insertedTo);
 		
 		if (selected == -1) {
 			selected = 0;
 		} else if (insertedTo <= selected) {
 			selected++;
 		}
-		
-		System.out.println(selected+" "+insertedTo);
 		_taskViewInterface.buildComponent(_taskManager.getWorkingList(), selected);
 		update(0);
 		
