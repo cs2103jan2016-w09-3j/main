de0e40efc8f5250f13e9148c803a5661aaa01c9a
Ten Zhi Yang <tzyinc@hotmail.com>
2016-03-10 18:40:13 +0800

    Refractor: added comments and moved get first word to parserCommons

--- test/src/dateParser/CommandParser.java
+++ test/src/dateParser/CommandParser.java
@@ -1,7 +1,6 @@
 package dateParser;
 
 public class CommandParser {
-	private static final int FIRST_WORD = 0;
 
 	public enum COMMAND {
 		ADD, EDIT, DELETE, EXIT, INVALID;
@@ -11,28 +10,33 @@ public class CommandParser {
 
 	}
 
-	private String getFirstWord(String input) {
-		String returnStrVal = input.trim().split("\\s+")[FIRST_WORD];
-		return returnStrVal;
-	}
-
+	/**
+	 * takes in a string and retuns the related command
+	 * @param input
+	 * @return COMMAND enum
+	 */
 	public COMMAND getCommand(String input) {
-		String inputCmd = getFirstWord(input);
+		String inputCmd = ParserCommons.getFirstWord(input);
 		COMMAND returnVal = COMMAND.INVALID;
 		if (inputCmd.equalsIgnoreCase("add")) {
 			returnVal = COMMAND.ADD;
-		}else if (inputCmd.equalsIgnoreCase("edit")) {
+		} else if (inputCmd.equalsIgnoreCase("edit")) {
 			returnVal = COMMAND.EDIT;
-		}else if (inputCmd.equalsIgnoreCase("delete")) {
+		} else if (inputCmd.equalsIgnoreCase("delete")) {
 			returnVal = COMMAND.DELETE;
-		}else if (inputCmd.equalsIgnoreCase("exit")) {
+		} else if (inputCmd.equalsIgnoreCase("exit")) {
 			returnVal = COMMAND.EXIT;
 		}
 		return returnVal;
 	}
 
+	/**
+	 * takes the first word and adds XML to it
+	 * @param input
+	 * @return xml of input
+	 */
 	public String xmlFirstWord(String input) {
-		String inputFirstWord = getFirstWord(input);
+		String inputFirstWord = ParserCommons.getFirstWord(input);
 		String returnStrVal = input;
 		returnStrVal = input.replace(inputFirstWord, "<cmd>" + inputFirstWord + "</cmd>");
 		return returnStrVal;
--- test/src/dateParser/ParserCommons.java
+++ test/src/dateParser/ParserCommons.java
@@ -1,13 +1,19 @@
 package dateParser;
 
 public class ParserCommons {
-	public static String getLastWord(String input){
-			String returnVal = null;
-			String [] seperated = input.split(" ");
-			if (seperated.length>0){
-				returnVal = seperated[seperated.length-1];
-			}
-			return returnVal;
+	private static final int FIRST_WORD = 0;
+	
+	public static String getLastWord(String input) {
+		String returnVal = null;
+		String[] seperated = input.split(" ");
+		if (seperated.length > 0) {
+			returnVal = seperated[seperated.length - 1];
 		}
+		return returnVal;
+	}
 
+	public static String getFirstWord(String input) {
+		String returnStrVal = input.trim().split("\\s+")[FIRST_WORD];
+		return returnStrVal;
+	}
 }
