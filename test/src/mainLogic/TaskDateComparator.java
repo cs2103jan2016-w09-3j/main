2f7007f8b69113e479f935747e7921ee6dfb8855
qy <qinying1313@yahoo.com>
2016-03-21 20:07:33 +0800

    Added sorting for floating task in TaskDatComparator Added Task Associations in TaskEntity

--- test/src/mainLogic/TaskDateComparator.java
+++ test/src/mainLogic/TaskDateComparator.java
@@ -8,24 +8,34 @@ import entity.TaskEntity;
 public class TaskDateComparator implements Comparator<TaskEntity> {
     public int compare(TaskEntity task1, TaskEntity task2) {
         if (task1.isFloating() || task2.isFloating()) {
-            System.out.println("Error at TaskDateComparator, dateless tasks being compared");
+            return compareFloating(task1, task2);
+        } else {        
+            Calendar date1 = task1.getDueDate();
+            Calendar date2 = task2.getDueDate();
+    
+            // 0 being returned means both tasks have the same date on the calendar.
+            // Terminate if different days, carry on sorting them if they are the
+            // same day
+            int differentDayResult = compareDifferentDay(date1, date2);
+            if( differentDayResult != 0){
+                return differentDayResult;
+            }
+    
+            return compareFullDayTask(task1, task2, date1, date2);
         }
-        
-        Calendar date1 = task1.getDueDate();
-        Calendar date2 = task2.getDueDate();
-
-        // 0 being returned means both tasks have the same date on the calendar.
-        // Terminate if different days, carry on sorting them if they are the
-        // same day
-        int differentDayResult = checkDifferentDay(date1, date2);
-        if( differentDayResult != 0){
-            return differentDayResult;
-        }
-
-        return checkFullDayTask(task1, task2, date1, date2);
     }
 
-    private int checkFullDayTask(TaskEntity task1, TaskEntity task2, Calendar date1, Calendar date2) {
+    private int compareFloating(TaskEntity task1, TaskEntity task2) {
+        if(task1.isFloating() && !task2.isFloating()){
+            return -1;
+        }else if(!task1.isFloating() && task2.isFloating()){
+            return 1;
+        }else{
+            return task1.getName().compareToIgnoreCase(task2.getName());
+        }
+    }
+
+    private int compareFullDayTask(TaskEntity task1, TaskEntity task2, Calendar date1, Calendar date2) {
         if (task1.isFullDay() && !task2.isFullDay()) {
             return -1;
         } else if (!task1.isFullDay() && task2.isFullDay()) {
@@ -34,11 +44,11 @@ public class TaskDateComparator implements Comparator<TaskEntity> {
             return date1.compareTo(date2);
         } else {
             // If both tasks are full day, sort them alphabetically
-            return task1.getName().toLowerCase().compareTo(task2.getName().toLowerCase());
+            return task1.getName().compareToIgnoreCase(task2.getName());
         }
     }
 
-    private int checkDifferentDay(Calendar date1, Calendar date2) {
+    private int compareDifferentDay(Calendar date1, Calendar date2) {
         if (date1.get(Calendar.YEAR) > date2.get(Calendar.YEAR)) {
             return 1;
         } else if (date1.get(Calendar.YEAR) < date2.get(Calendar.YEAR)) {
