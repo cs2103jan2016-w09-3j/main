ce2de49c479719ca6fdc41f5a55fc6f7652d3dc7
Ten Zhi Yang <tzyinc@hotmail.com>
2016-03-22 16:41:51 +0800

    added Date{arserTest

---
+++ test/src/test/dateParser/DateParserTest.java
@@ -0,0 +1,30 @@
+package test.dateParser;
+import dateParser.DateParser;
+import static org.junit.Assert.*;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Test;
+
+public class DateParserTest {
+	DateParser dp = new DateParser();
+	List<Date> dates = dp.parseToList("01/02/2016 1500hrs");
+	Date testDate = dates.get(0);
+	@Test
+	public void testParserToList() {
+		Calendar c = Calendar.getInstance();
+		c.set(2016, 01, 01, 15, 00, 00);
+		c.clear(Calendar.MILLISECOND);
+		assertEquals(0, c.getTime().compareTo(testDate));
+		
+	}
+	@Test
+	public void testXMLDate(){
+		assertEquals("<dates>tmr</dates> blah blah ",dp.xmlDate("tmr blah blah"));
+	}
+	
+	
+
+}
