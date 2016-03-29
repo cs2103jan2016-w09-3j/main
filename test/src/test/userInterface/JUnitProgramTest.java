127fc008c7f9ffde3541b234489d08c2f785cf1c
kazeshan <kazeshan@gmail.com>
2016-03-29 13:41:55 +0800

    add executeInterface

--- test/src/test/userInterface/JUnitProgramTest.java
+++ test/src/test/userInterface/JUnitProgramTest.java
@@ -2,24 +2,74 @@ package test.userInterface;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 import dateParser.InputParser;
+import entity.TaskEntity;
+import mainLogic.TaskManager;
+import mainLogic.Utils;
 import userInterface.CommandBar;
+import userInterface.UserInterfaceExecuter;
 import dateParser.CommandParser.COMMAND;
 
 public class JUnitProgramTest {
 
+	UserInterfaceExecuter ex;
+
 	@Test
-	public void test() {
-		runCommand("asd");
+	public void testFullCommands() {
+		ex = new UserInterfaceExecuter();
+	}
+	
+	@Test
+	public void testAddCommands() {
+		ex = new UserInterfaceExecuter();
+		assertEquals(runCommand("add basktball"), -1);
+		assertEquals(runCommand("add basktball"), -1);
+		assertEquals(runCommand("add basktball"), -1);
+		ex.switchView(TaskManager.DISPLAY_FLOATING);
+		
+	}
+	
+	@Test
+	public void tesDeleteCommands() {
+		ex = new UserInterfaceExecuter();
 	}
 
-	public void runCommand(String rawString) {
+	public int runCommand(String rawString) {
 		InputParser parser = new InputParser(rawString);
 		COMMAND cmd = parser.getCommand();
-		if (cmd.equals(COMMAND.ADD)) {
-		} 
-		
+		switch (cmd) {
+		case ADD: {
+			ArrayList<TaskEntity> tasks = parser.getTask();
+			if (tasks.size() == 1) {
+				return ex.addTask(tasks.get(0), rawString);
+			} else {
+				return ex.addBatch(tasks, rawString);
+			}
+		}
+		case DELETE: {
+			String id = parser.getID();
+			return ex.delete(id, rawString);
+		}
+		case EDIT: {
+			String id = parser.getID();
+			parser.removeId();
+			ArrayList<TaskEntity> tasks = parser.getTask();
+			if (tasks.size() == 1) {
+				return ex.modify(Utils.convertBase36ToDec(id), tasks.get(0), rawString);
+			} else {
+				return -1;
+			}
+		}
+		case DONE: {
+			String id = parser.getID();
+			return ex.markAsDone(Utils.convertBase36ToDec(id), rawString);
+		}
+		default:
+			return -9;
+		}
 	}
 }
---
+++ test/src/userInterface/UserInterfaceExecuter.java
@@ -0,0 +1,68 @@
+package userInterface;
+
+import java.util.ArrayList;
+import java.util.Queue;
+
+import entity.TaskEntity;
+import mainLogic.TaskManagerInterface;
+
+public class UserInterfaceExecuter {
+
+	private TaskManagerInterface _taskManager = new TaskManagerInterface();
+
+	public ArrayList<TaskEntity> getWorkingList() {
+		return _taskManager.getWorkingList();
+	}
+
+	public int getNextTimeListId() {
+		return _taskManager.getNextTimeListId();
+	}
+
+	public TaskEntity getRandomFloating() {
+		return _taskManager.getRandomFloating();
+	}
+
+	public void switchView(int displaySearch) {
+		_taskManager.switchView(displaySearch);
+	}
+
+	// commands execution //
+
+	public int addTask(TaskEntity task, String rawCommandWithView) {
+		int insertedTo = _taskManager.add(task, rawCommandWithView);
+		return insertedTo;
+	}
+
+	public int addBatch(ArrayList<TaskEntity> task, String rawCommandWithView) {
+		return _taskManager.add(task, rawCommandWithView);
+	}
+
+	public int delete(String id, String rawCommandWithView) {
+		return _taskManager.delete(id, rawCommandWithView);
+	}
+
+	public int modify(int idToModify, TaskEntity task, String rawCommandWithView) {
+		return _taskManager.modify(idToModify, task, rawCommandWithView);
+	}
+
+	public int searchString(String stringToSearch, String rawCommandWithView) {
+		return _taskManager.searchString(stringToSearch, rawCommandWithView);
+	}
+
+	public int markAsDone(int indexInt, String rawCommandWithView) {
+		return _taskManager.markAsDone(indexInt, rawCommandWithView);
+	}
+
+	public boolean link(TaskEntity taskEntity, TaskEntity taskEntity2, String rawCommandWithView) {
+		return _taskManager.link(taskEntity, taskEntity2, rawCommandWithView);
+	}
+
+	public void closeTaskManager() {
+		_taskManager.closeTaskManager();
+	}
+
+	public Queue<String> getBackedupCommands() {
+		return _taskManager.getBackedupCommands();
+	}
+
+}
