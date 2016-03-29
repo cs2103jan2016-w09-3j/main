4d7d8867905d5c8af24d80b74353c52a4a43c8bb
Ten Zhi Yang <tzyinc@hotmail.com>
2016-03-29 10:41:59 +0800

    Refrector: work around with xml tags getting tagged

--- test/src/dateParser/XMLParser.java
+++ test/src/dateParser/XMLParser.java
@@ -14,13 +14,13 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 
 public class XMLParser {
-	public static final String DATE_TAG = "dates";
-	public static final String CMD_TAG = "cmd";
-	public static final String TITLE_TAG = "title";
-	public static final String DESC_TAG = "desc";
-	public static final String OTHERS_TAG = "others";
-	public static final String ID_TAG = "ID";
-	public static final String HASH_TAG = "hashtags";
+	public static final String DATE_TAG = "DaTeSxMl";
+	public static final String CMD_TAG = "CmDxMl";
+	public static final String TITLE_TAG = "TiTlExMl";
+	public static final String DESC_TAG = "DeScXmL";
+	public static final String OTHERS_TAG = "OtHeRxMl";
+	public static final String ID_TAG = "IdXmL";
+	public static final String HASH_TAG = "HaShXmL";
 	
 	public static Document loadXMLFromString(String xml) throws Exception{
 	    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
