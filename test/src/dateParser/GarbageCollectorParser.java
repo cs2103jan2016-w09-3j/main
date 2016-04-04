f9566a2f9a2e206a4d447ac92d60dbc199fbd775
Ten Zhi Yang <tzyinc@hotmail.com>
2016-04-04 16:07:09 +0800

    added comments for collate

--- test/src/dateParser/GarbageCollectorParser.java
+++ test/src/dateParser/GarbageCollectorParser.java
@@ -3,54 +3,66 @@ package dateParser;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * 
+ * @@author :a0125415n
+ *
+ */
 public class GarbageCollectorParser {
 	private ArrayList<String> hashes= new ArrayList<String>();
 	public GarbageCollectorParser(){
 	}
 	
+	/**@@author :a0125415n
+	 * 
+	 * adds xml to anything that has hashtags
+	 * @param String input
+	 * @return input with xml around any word with has in front
+	 */
 	public String xmlHash(String input){
 		String toReplace = XMLParser.removeAllAttributes(input);
 		hashes = new ArrayList<String>();
-		//System.out.println("GC"+toReplace);
 		Scanner sc = new Scanner(toReplace);
 		while(sc.hasNext()){
 			String temp = sc.next();
-			//System.out.println("GC2"+input);
 			if((temp.length()!=1)&&(!temp.contains("ID"))){
 				if(temp.charAt(0)=='#'){
 					hashes.add(temp);
 					input = input.replace(temp, "<"+XMLParser.HASH_TAG+">"+temp+"</"+XMLParser.HASH_TAG+">");
 				}	
 			}
-			//System.out.println("test2");
 		}
-		
 		return input;
 	}
 	
+	/**@@author :a0125415n
+	 * 
+	 * get the hashes in the string, precondition: run xmlHash first
+	 * @return ArrayList of hashes in string
+	 */
 	public ArrayList<String> getHashes(){
 		return hashes;
 	}
 	
+	/**@@author :a0125415n
+	 * 
+	 * add xml to anything that hasn't already got xml.
+	 * @param String input
+	 * @return input with xml on any other not yet defined fields
+	 */
 	public String xmlAllOthers(String input){
-		//System.out.println(input);
 		String toReplace = XMLParser.removeAllAttributes(input);
-		//System.out.println("GC"+toReplace);
 		Scanner sc = new Scanner(toReplace);
 		while(sc.hasNext()){
 			String temp = sc.next();
-			//System.out.println("GC2"+input);
 			if((temp.length()!=1)&&(!temp.contains("ID"))){
 					input = input.replace(temp, "<"+XMLParser.OTHERS_TAG+">"+temp+"</"+XMLParser.OTHERS_TAG+">");
 					
 			}
-			//System.out.println("test2");
 		}
 		String lastWord = ParserCommons.getLastWord(input);
 		String firstWord = ParserCommons.getFirstWord(XMLParser.removeAllTags(input));
-		//System.out.println(firstWord+" , "+lastWord);
 		if((lastWord.length()==1)&&(!firstWord.equals(lastWord))&&(!lastWord.contains("ID"))){
-			//System.out.println("test");
 			input = input.substring(0, input.length()-2)+"<"+XMLParser.OTHERS_TAG+">"+lastWord+"</"+XMLParser.OTHERS_TAG+">";
 		}
 		return input;
