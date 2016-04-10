d0605fe940a0fc246544cdf5b3c4fedf9e5da9a2
Ten Zhi Yang <tzyinc@hotmail.com>
2016-04-10 14:18:20 +0800

    added test case

---
+++ test/src/test/dateParser/IdParserTest.java
@@ -0,0 +1,56 @@
+//@@author a0125415n
+package test.dateParser;
+import dateParser.CommandParser;
+import dateParser.IdParser;
+import dateParser.Pair;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class IdParserTest {
+	IdParser id = new IdParser();
+	
+	@Test
+	public void testIDParser_GetID(){
+		String toTest = id.getID("ID123");
+		String expected = "123";
+		assertEquals(toTest,expected);
+	}
+	
+	@Test
+	public void testIDParser_GetID_noInput(){
+		String toTest = id.getID("asfd");
+		String expected = null;
+		assertEquals(toTest,expected);
+	}
+	
+	@Test
+	public void testIDParser_GetLinkID(){
+		Pair<String,String> toTest = id.getLinkID("ID123-ID234");
+		Pair<String,String> expected = new Pair("123","234");
+		assertEquals(toTest.getFirst(),expected.getFirst());
+		assertEquals(toTest.getSecond(),expected.getSecond());
+	}
+	
+	@Test
+	public void testIDParser_GetLinkID_invalidInput(){
+		Pair<String,String> toTest = id.getLinkID("123-234");
+		Pair<String,String> expected = new Pair(null,null);
+		assertEquals(toTest.getFirst(),expected.getFirst());
+		assertEquals(toTest.getSecond(),expected.getSecond());
+	}
+	
+	@Test
+	public void testIDParser_xmlID(){
+		String toTest = id.xmlID("ID1234");
+		String expected = "<IdXmL>ID1234</IdXmL>";
+		assertEquals(toTest,expected);
+	}
+	
+	@Test
+	public void testIDParser_xmlID_invalidInput(){
+		String toTest = id.xmlID("1234");
+		String expected = "1234";
+		assertEquals(toTest,expected);
+	}
+}
